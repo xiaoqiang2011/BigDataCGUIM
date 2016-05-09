84b82fef5eff5cf248d9c858e33b56d6e1bf1cf0
yijutseng <beergt@gmail.com>
2016-05-09 22:51:58 +0800

    0516-Pre

---
+++ Hadoop.R
@@ -0,0 +1,6 @@
+Depends: R (>= 2.6.0), methods
+Imports: Rcpp, RJSONIO (>= 0.8-2), digest, functional, reshape2, stringr, plyr, caTools (>= 1.16)
+Suggests: quickcheck (>= 3.1.0), ravro, rhdfs, testthat
+
+install.packages(c("methods","Rcpp", "RJSONIO", "digest", "functional", "reshape2",
+                   "stringr", "plyr", "caTools","quickcheck","testthat"))
