b12509da1ad6876b160182bdea37ccc17274ca5d
yijutseng <beergt@gmail.com>
2016-05-09 22:57:55 +0800

    Hadoop

--- Hadoop.R
+++ Hadoop.R
@@ -1,6 +1,3 @@
-Depends: R (>= 2.6.0), methods
-Imports: Rcpp, RJSONIO (>= 0.8-2), digest, functional, reshape2, stringr, plyr, caTools (>= 1.16)
-Suggests: quickcheck (>= 3.1.0), ravro, rhdfs, testthat
-
 install.packages(c("methods","Rcpp", "RJSONIO", "digest", "functional", "reshape2",
-                   "stringr", "plyr", "caTools","quickcheck","testthat"))
+                   "stringr", "plyr", "caTools","quickcheck","testthat"),
+                 dependencies=TRUE, repos='http://cran.rstudio.com/')
