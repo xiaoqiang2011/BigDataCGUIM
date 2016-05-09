1bb032f4260a8d2971fa215a8f60b033d37938d8
yijutseng <beergt@gmail.com>
2016-05-09 23:19:58 +0800

    plyrmr

--- Hadoop.R
+++ Hadoop.R
@@ -1,3 +1,12 @@
 install.packages(c("methods","Rcpp", "RJSONIO", "digest", "functional", "reshape2",
                    "stringr", "plyr", "caTools","quickcheck","testthat"),
                  dependencies=TRUE, repos='http://cran.rstudio.com/')
+
+install.packages(c("dplyr","R.methodsS3","Hmisc", "stats", "memoise", "lazyeval", "rjson"),
+                 dependencies=TRUE, repos='http://cran.rstudio.com/')
+
+
+
+rJAVA?
+
+sudo R CMD INSTALL rmr2_2.3.0.tar.gz
