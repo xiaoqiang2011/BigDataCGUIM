1aba5cfacc71e3c9b5ba201e3b6723d89827c290
yijutseng <beergt@gmail.com>
2016-05-10 21:48:18 +0800

    typo

--- Hadoop.R
+++ Hadoop.R
@@ -9,7 +9,7 @@ https://github.com/RevolutionAnalytics/RHadoop/wiki/Installing-RHadoop-on-RHEL
 RHadoop
 http://unix.stackexchange.com/questions/271514/setting-persistent-environment-variable-in-centos-7-issue
 echo export HADOOP_CMD="/usr/bin/hadoop">/etc/profile.d/hadoopenv.sh
-echo export HADOOP_STREAMING="/opt/cloudera/parecels/CDH-5.4.5.1.cdh5.4.5.p0.7/jars/hadoop-streaming-2.6.0-mr1-cdh5.4.5.jar"> /etc/profile.d/hadoopenv.sh
+echo export HADOOP_STREAMING="/opt/cloudera/parcels/CDH-5.4.5.1.cdh5.4.5.p0.7/jars/hadoop-streaming-2.6.0-mr1-cdh5.4.5.jar"> /etc/profile.d/hadoopenv.sh
 chmod 0755 /etc/profile.d/hadoopenv.sh
 
 rmr2
@@ -37,8 +37,9 @@ install.packages("rJava",
 sudo HADOOP_CMD=/usr/bin/hadoop R CMD INSTALL rhdfs_1.0.8.tar.gz
 
 R 指令
+Sys.setenv(HADOOP_HOME="/usr/bin/hadoop")
 Sys.setenv(HADOOP_CMD="/usr/bin/hadoop")
-Sys.setenv(HADOOP_STREAMING="/opt/cloudera/parecels/CDH-5.4.5.1.cdh5.4.5.p0.7/jars/hadoop-streaming-2.6.0-mr1-cdh5.4.5.jar")
+Sys.setenv(HADOOP_STREAMING="/opt/cloudera/parcels/CDH-5.4.5.1.cdh5.4.5.p0.7/jars/hadoop-streaming-2.6.0-mr1-cdh5.4.5.jar")
                 
 http://192.168.128.11:8787/
 
