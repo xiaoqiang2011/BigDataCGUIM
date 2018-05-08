e8abd63c75aa2c33458655a4d9031a370c0b97ec
Yi-Ju Tseng <beergt@gmail.com>
2018-05-08 12:00:08 +0800

    Update 0508.R

--- 106/0508.R
+++ 106/0508.R
@@ -1,26 +1,67 @@
-library(ggplot2) 
-qplot(Sepal.Length,Sepal.Width,data=iris,color=Species)
+library(SportsAnalytics)
+library(data.table)
 
-qplot(Sepal.Length,Sepal.Width,data=iris,color=Species)+theme_bw()
+NBA1617<-
+  fetch_NBAPlayerStatistics("16-17")
+
+nrow(NBA1617[NBA1617$TotalPoints>2000,])
+
+NBA1617DT<-data.table(NBA1617)
+nrow(NBA1617DT[TotalPoints>2000])
 
 
-qplot(Petal.Width, data=iris, fill=Species,binwidth=0.5)
+NBA1617DT[GamesPlayed>50,
+          .(StealsMean=mean(Steals),
+            TReboundsMean=mean(TotalRebounds)),
+          by=Position]
+
+NBA1617DT%>%
+  filter(GamesPlayed>50)%>%
+  group_by(Position)%>%
+  summarise(StealsMean=mean(Steals),
+            TReboundsMean=mean(TotalRebounds))
+
+NBA1617DT[TotalPoints>2000,
+          .(StealsMean=mean(Steals),
+            TReboundsMean=mean(TotalRebounds))]
+
+library(dplyr)
+NBA1617DT %>%
+  filter(TotalPoints>2000) %>%
+  summarise(StealsMean=mean(Steals),
+            TReboundsMean=mean(TotalRebounds))
+
+
 
 library(SportsAnalytics)
-NBA1516<-fetch_NBAPlayerStatistics("15-16") 
-qplot(FieldGoalsAttempted, TotalPoints, 
-      data = NBA1516,
-      facets = Position ~ Team)
-
-qplot(Sepal.Length,Sepal.Width,data=iris,facets = .~ Species)
-
+NBA1617<-fetch_NBAPlayerStatistics("16-17")
 
 library(ggplot2) 
-ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width)) + 
-  geom_point()+
-  facet_grid(.~Species)
+qplot(Steals,TotalRebounds, 
+      data = NBA1617)
+
+cor(NBA1617$Steals,NBA1617$TotalRebounds)
 
 
-ggplot(iris, aes(Species,Sepal.Width))+
-  geom_boxplot()+
-  theme_void()
+library(readr)
+file <- read_csv("C:/Users/Administrator/Downloads/201803.csv")
+
+file$縣市 <- 
+  substr(file$公司所在地, 
+         start = 1, stop = 3)
+library(dplyr)
+Info <- file%>%
+  mutate(縣市=substr(公司所在地,start = 1, stop = 3))
+  group_by(縣市)%>%
+  summarise(公司數目=n(),
+                平均資本額 = mean(資本額))
+#------------------------------------------------------------------------
+Data201803 <- read_csv("C:/Users/Administrator/Downloads/201803.csv")
+Data201803$City <- substr(Data201803$公司所在地,1,3)
+Data201803DT <- data.table(Data201803)
+b <- Data201803DT[,
+                  .(CompanyNumber = .N,
+                    MeanMoney = mean(資本額)),
+                  by = City]
+
+
