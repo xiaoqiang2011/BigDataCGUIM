8eb5f8221bcafb0c09473e90d5bf4b0cea8fc1bf
Yi-Ju Tseng <beergt@gmail.com>
2018-05-22 12:13:51 +0800

    Update 0522.R

--- 106/0522.R
+++ 106/0522.R
@@ -1,41 +1,81 @@
-nba <- read.csv("http://datasets.flowingdata.com/ppg2008.csv")
-head(nba,3)
-library(reshape2) #for melt()
-#寬表轉長表,以名字作依據
-nba.m <- melt(nba,id.vars = "Name") 
-head(nba.m,5)
+library(jsonlite)
+BikeData<-fromJSON("https://data.tycg.gov.tw/opendata/datalist/datasetMeta/download?id=5ca2bfc7-9ace-4719-88ae-4034b9a5a55c&rid=a1b4714b-3b75-4ff8-a8f2-cc377e4eaa0f")
 
-library(readr) 
+
+BikeDataDF <- data.frame(
+  matrix(unlist(BikeData$retVal), 
+         nrow=length(BikeData$retVal), byrow=T),
+  stringsAsFactors=FALSE)
 library(ggplot2)
-POLIO_Incidence <- 
-  read_csv("https://raw.githubusercontent.com/CGUIM-BigDataAnalysis/BigDataCGUIM/master/104/POLIO_Incidence.csv")
+library(ggmap)
 
-POLIO_Incidence.m<-
-  melt(POLIO_Incidence, id.vars = c("YEAR", "WEEK"))
+t<-get_googlemap(center=c(lon=121.20,lat=25.00),
+                 zoom = 11,language="zh-TW")
 
-POLIO_Incidence.m$value<-gsub("-",NA,POLIO_Incidence.m$value)
+ggmap(t)
 
-POLIO_Incidence.m$value<-as.numeric(POLIO_Incidence.m$value)
+BikeDataDF$X5<-as.numeric(BikeDataDF$X5)
+BikeDataDF$X4<-as.numeric(BikeDataDF$X4)
+BikeDataDF$X3<-as.numeric(BikeDataDF$X3)
+
+n<-ggmap(t)+
+  geom_point(data=BikeDataDF,
+             aes(x=X5, y=X4,size=X3),
+             color="red")+
+  scale_size(range = c(1,10))
+n
 
 
-ggplot(POLIO_Incidence.m, aes(YEAR, variable)) + 
-  geom_tile(aes(fill = value),
-            colour = "white")+ 
-  scale_fill_gradient(
-    low = "white",high = "steelblue") 
+
+library(readr)
+Dengue <- 
+  read_csv("https://od.cdc.gov.tw/eic/Dengue_Daily.csv")
+
+
+Dengue $最小統計區中心點X<-
+  as.numeric(Dengue $最小統計區中心點X)
+Dengue $最小統計區中心點Y<-
+  as.numeric(Dengue $最小統計區中心點Y)
+Dengue$確定病例數<-
+  as.numeric(Dengue $確定病例數)
+
+WrongData<-data.frame(lon=Dengue $最小統計區中心點X,
+           lat=Dengue $最小統計區中心點Y,
+           stringsAsFactors=F)
+
+WrongData$lon<-as.numeric(as.character(WrongData$lon))
+WrongData$lat
 
 library(dplyr)
-POLIO_Incidence.m.y<-
-  POLIO_Incidence.m %>% group_by(YEAR,variable) %>% 
-  summarize(valueSum=sum(value,na.rm = T))
+df<-filter(Dengue,Dengue$確定病例數>1.0)
+Dengue<-rbind(Dengue,df)
 
 
-polioPlot<-ggplot(POLIO_Incidence.m.y, aes(YEAR, variable)) + 
-  geom_tile(aes(fill = valueSum),
-            colour = "white")+ 
-  scale_fill_gradient(
-    low = "white",high = "steelblue") +
-  theme_minimal()
+library(ggmap)
+twmap <- get_googlemap(center = c(lon=120.58,lat=23.58), 
+                       zoom = 8,
+                       language = "zh-TW")
 
-library(plotly)
-ggplotly(polioPlot)
+ggmap(twmap, extent = "device")+
+  geom_density2d(data = Dengue, aes(x = 最小統計區中心點X, 
+                                    y =最小統計區中心點Y), 
+                 size = 1) + 
+  stat_density2d(data = Dengue, 
+                 aes(x = 最小統計區中心點X, 
+                     y = 最小統計區中心點Y, 
+                     fill = ..level.., alpha = ..level..), 
+                 size = 0.01, bins = 16, geom = "polygon") + 
+  scale_fill_gradient(low = "green", 
+                      high = "red", guide = FALSE) + 
+  scale_alpha(range = c(0, 0.3), guide = FALSE)
+
+
+Dengue %>% group_by(居住縣市) %>% 
+  summarise(Count=n()) %>% 
+  arrange(desc(Count))
+
+
+
+
+
+
